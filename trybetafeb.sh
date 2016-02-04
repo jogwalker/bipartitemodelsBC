6b17c11d06a56ee55d05058999fe841f7eb49eba
Josephine Walker <jogwalker@gmail.com>
2016-02-04 11:57:04 +0000

    trial run for beta regression on model1

---
+++ trybetafeb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#PBS -N jgwZINBfeb
+#PBS -o output/beta
+#PBS -e error/beta
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -S /bin/bash
+#PBS -t 0
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  PBS_ARRAYID=${1}
+fi
+
+filepath="$HOME/bipartitemodelsBC/finalmodels/beta"
+outdir="~/bipartitemodelsBC/results/beta"
+iter=3000
+burnin=100
+thin=1
+
+ls -1 ${filepath}
+
+i=0
+while read line
+do
+    array[ $i ]="$line"        
+    (( i++ ))
+done < <(ls -1 ${filepath})
+
+echo ${filepath}/${array[${PBS_ARRAYID}]}
+
+R --no-save --args ${outdir} ${runlength[${PBS_ARRAYID}]} ${iter} ${burnin} ${thin} < ${filepath}/${array[${PBS_ARRAYID}]}
